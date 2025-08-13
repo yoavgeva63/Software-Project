b1fea73994251a32f744e00ee628c9cf9851f67a
yoavgeva63 <geva.yoav@gmail.com>
2025-08-13 15:03:31 +0000

    only setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import Extension, setup
+
+module = Extension("symnmf", sources=['symnmfmodule.c', 'symnmf.c'])
+setup(name='symnmf',
+     version='1.0',
+     description='Python wrapper for symnmf C extension',
+     ext_modules=[module])
